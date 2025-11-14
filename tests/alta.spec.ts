556a03cd55679c003e5d0fa234a6765f5b660715
John Bolorinos <jkbolorinos@gmail.com>
2025-11-14 18:43:31 +0100

    add basic UI visibility assertions for page load

--- tests/alta.spec.ts
+++ tests/alta.spec.ts
@@ -1,7 +1,11 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Pagatelia Registration", () => {
-  test("Page loads", async ({ page }) => {
+  test("Page loads and main elements exist", async ({ page }) => {
     await page.goto("https://development.pagatelia.com/alta/");
+
+    await expect(page.locator("header")).toBeVisible();
+    await expect(page.locator("img")).toBeVisible(); // placeholder
+    await expect(page.getByText("Datos")).toBeVisible(); // temporary selector
   });
 });
